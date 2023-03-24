718718ea24051d5d106f1513cfc9e3eaa2445a2d
Vigneshk5 <vigneshpatali5@gmail.com>
2023-03-24 22:10:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
